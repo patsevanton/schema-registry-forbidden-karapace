d7f5af9cf3bd0c581980a811403c0b3a0135d8a9
Anton Patsev <patsev.anton@gmail.com>
2026-09-16 10:18:54 +0600

    fix: выбор первого хоста для bootstrap-эндпоинта Kafka

--- locals.tf
+++ locals.tf
@@ -6,6 +6,6 @@ locals {
 
   # Bootstrap-хост Kafka и REST-эндпоинт Karapace (managed schema registry)
   # доступны на одном и том же broker-хосте.
-  kafka_host          = one(yandex_mdb_kafka_cluster.this.host).name
+  kafka_host          = sort([for h in yandex_mdb_kafka_cluster.this.host : h.name])[0]
   schema_registry_url = "https://${local.kafka_host}:443"
 }
