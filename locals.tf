8b27a4bfc8c4996922349f83171df46d3e0fcf63
Anton Patsev <patsev.anton@gmail.com>
2026-09-16 12:34:22 +0600

    fix: выбор брокера role=KAFKA для bootstrap-хоста Kafka

--- locals.tf
+++ locals.tf
@@ -5,7 +5,11 @@ locals {
   subnet_zone = yandex_vpc_subnet.this[var.zone].zone
 
   # Bootstrap-хост Kafka и REST-эндпоинт Karapace (managed schema registry)
-  # доступны на одном и том же broker-хосте.
-  kafka_host          = sort([for h in yandex_mdb_kafka_cluster.this.host : h.name])[0]
+  # доступны на одном и том же broker-хосте. В кластере есть хосты ZooKeeper,
+  # которые не слушают 9091/443, поэтому выбираем именно брокер (role=KAFKA).
+  kafka_host = sort([
+    for h in yandex_mdb_kafka_cluster.this.host : h.name
+    if h.role == "KAFKA"
+  ])[0]
   schema_registry_url = "https://${local.kafka_host}:443"
 }
